7668193a2ceb9727bf742c6c6df9a78947af1feb
Ruihao Huang <huangruihaocst@126.com>
2019-10-02 16:37:55 -0700

    [add] Add template for heap.

---
+++ heap.py
@@ -0,0 +1,15 @@
+class Heap:
+    def __init__(self):
+        pass
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
---
+++ heap_test.py
@@ -0,0 +1,14 @@
+import unittest
+from heap import Heap
+
+
+class HeapTest(unittest.TestCase):
+
+    def setUp(self):
+        self.heap = Heap()
+
+    def tearDown(self):
+        pass
+
+    def test(self):
+        pass
